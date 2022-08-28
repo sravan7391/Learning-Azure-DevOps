53c22852922501328bd33008e0860e051137858c
Sravan kumar <sravan.dme91@gmail.com>
2022-08-28 19:20:42 +0000

    Updated VM_Creation.ps1

--- VM_Creation.ps1
+++ VM_Creation.ps1
@@ -20,21 +20,21 @@ Write-Host "=========================================================="
 
 #Checking resourcegroup status. if exits or not
  $VMStatus =  az vm list -d -o table --query "[?name=='$VmName']"
-# if($VMStatus -eq $true){
-#      Write-Host $VMStatus
-# }
-# else{
-#      Write-Host creating VM
-#     az vm create --resource-group $RGName --name $VmName --image $Urn --vnet-name $VnetName --subnet $SubnetName --admin-username $Username --admin-password $Password --size $VmSize
-#  }
-
-if($VMStatus -eq $false){
-   Write-Host creating VM
-   az vm create --resource-group $RGName --name $VmName --image $Urn --vnet-name $VnetName --subnet $SubnetName --admin-username $Username --admin-password $Password --size $VmSize
+if($VMStatus -eq $true){
+     Write-Host $VMStatus
 }
 else{
-   az vm list -d -o table --query "[?name=='$VmName']"
-}
+     Write-Host creating VM
+    az vm create --resource-group $RGName --name $VmName --image $Urn --vnet-name $VnetName --subnet $SubnetName --admin-username $Username --admin-password $Password --size $VmSize
+ }
+
+# if($VMStatus -eq $false){
+#    Write-Host creating VM
+#    az vm create --resource-group $RGName --name $VmName --image $Urn --vnet-name $VnetName --subnet $SubnetName --admin-username $Username --admin-password $Password --size $VmSize
+# }
+# else{
+#    az vm list -d -o table --query "[?name=='$VmName']"
+# }
 #==================================================================
 
 Write-Host "----------------------------------------------------"
