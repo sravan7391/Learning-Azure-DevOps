690edde5fbbba976c3e9b3bf812051b31f8a04b2
Sravan kumar <sravan.dme91@gmail.com>
2022-08-28 08:32:18 +0000

    Added VM_Creation.ps1

---
+++ VM_Creation.ps1
@@ -0,0 +1,32 @@
+param(
+    [Parameter(Mandatory=$true)][string] $RGName,
+    [Parameter(Mandatory=$true)][string] $VMImage,
+    [Parameter(Mandatory=$true)][string] $RVnetName,
+    [Parameter(Mandatory=$true)][string] $SubnetName,
+    [Parameter(Mandatory=$true)][string] $URN,
+    [Parameter(Mandatory=$true)][string] $VMUname,
+    [Parameter(Mandatory=$true)][string] $VMPwd,
+    [Parameter(Mandatory=$true)][string] $VMSize
+ 
+)
+$ErrorActionPreference  = "stop"
+[console]::ResetColor()
+$ScriptName = $MyInvocation.MyCommand.Name
+
+Write-host "----------------------------------------------------"
+Write-Host resourcegroup location is $RGLocation
+Write-Host resourcegroup name is $RGName
+Write-host "----------------------------------------------------"
+#Check RG Status
+$RGStatus = az network exists -n $RGName
+
+if ($RGStatus -eq $true)
+{
+    az vm create --resource-group HUB-RG --name HUB_WINVM --image $URN --vnet-name $RVnetName --subnet $SubnetName --admin-username $VMUname --admin-password $VMPwd --size $VMSize
+}
+else{
+    Write-Host Network $RGStatus already exists
+}
+
+#======================
+Write-Host "End of Script ($ScriptName)"
