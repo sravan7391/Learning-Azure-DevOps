10f132879a0c2e86c370acbe33ee105b8b647175
Sravan kumar <sravan.dme91@gmail.com>
2022-08-28 09:44:20 +0000

    Updated VM_Creation.ps1

--- VM_Creation.ps1
+++ VM_Creation.ps1
@@ -1,31 +1,41 @@
 param(
-    [Parameter(Mandatory=$true)][string] $RGName,
-    [Parameter(Mandatory=$true)][string] $VMImage,
-    [Parameter(Mandatory=$true)][string] $RVnetName,
-    [Parameter(Mandatory=$true)][string] $SubnetName,
-    [Parameter(Mandatory=$true)][string] $URN,
-    [Parameter(Mandatory=$true)][string] $VMUname,
-    [Parameter(Mandatory=$true)][string] $VMPwd,
-    [Parameter(Mandatory=$true)][string] $VMSize
-)
-$ErrorActionPreference  = "stop"
-[console]::ResetColor()
-$ScriptName = $MyInvocation.MyCommand.Name
+   [Parameter(Mandatory=$true)][string] $RGName,
+   [Parameter(Mandatory=$true)][string] $VnetName,
+   [Parameter(Mandatory=$true)][string] $SubnetName,
+   [Parameter(Mandatory=$true)][string] $Urn,
+   [Parameter(Mandatory=$true)][string] $VmName,
+   [Parameter(Mandatory=$true)][string] $Username,
+   [Parameter(Mandatory=$true)][string] $Password,
+   [Parameter(Mandatory=$true)][string] $VmSize
 
-Write-host "----------------------------------------------------"
-Write-Host resourcegroup name is $RGName
-Write-host "----------------------------------------------------"
-#Check RG Status
-#$RGStatus = az network exists -n $RVnetName
-Write-Host Hello VM 
-#az vm create --resource-group $RGName --name $VMImage --image $URN --vnet-name $RVnetName --subnet $SubnetName --admin-username $VMUname --admin-password $VMPwd --size $VMSize
-az vm create --resource-group $RGName --name $VMImage --image $URN --vnet-name $RVnetName --subnet $SubnetName --admin-username $VMUname --admin-password $VMPwd --size $VMSize
-# if ($RGStatus -eq $true)
-# {
-#     az vm create --resource-group $RGName --name $VMImage --image $URN --vnet-name $RVnetName --subnet $SubnetName --admin-username $VMUname --admin-password $VMPwd --size $VMSize
+)
+$ErrorActionPreference = "Stop"
+[Console]::ResetColor()
+$scriptName = $MyInvocation.MyCommand.Name
+write-host "========================================================="
+write-host resourcegroupname is $RGName
+write-host vnetname is $VnetName
+write-host SubnetName is $SubnetName
+write-host Urn is $Urn
+write-host VMname is $VmName
+write-host Username is $Username
+write-host Password is $Password
+write-host VMSize is $VmSize
+Write-Host "=========================================================="
+
+#Checking resourcegroup status. if exits or not
+# $vnetStatus = az network vnet exists -g $RGName -n $VnetName
+# if($vnetStatus -eq $false){
+    Write-Host creating VM
+    az vm create --resource-group $RGName --name $VmName --image $Urn --vnet-name $VnetName --subnet $SubnetName --admin-username $Username --admin-password $Password --size $VmSize
+
+
 # }
 # else{
-#     Write-Host Network $RGStatus already exists
+    # Write-Host vnet $VnetName already exists
 # }
-#======================
-# Write-Host "End of Script ($ScriptName)"
+#==================================================================
+
+Write-Host "----------------------------------------------------"
+Write-Host "-- script finished ($scriptName)"
+Write-Host "------------------------------------------"
