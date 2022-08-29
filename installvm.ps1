619c353991a85866e173da324a58201b5256cf7b
= <=>
2022-08-29 14:39:51 +0530

    code reverted

--- installvm.ps1
+++ installvm.ps1
@@ -1,53 +1,26 @@
-# param(
-#     [Parameter(Mandatory=$true)][string] $RGLocation,
-#     [Parameter(Mandatory=$true)][string] $RGName
-# )
-# $ErrorActionPreference  = "stop"
-# [console]::ResetColor()
-# $ScriptName = $MyInvocation.MyCommand.Name
-
-# Write-host "----------------------------------------------------"
-# Write-Host resourcegroup location is $RGLocation
-# Write-Host resourcegroup name is $RGName
-# Write-host "----------------------------------------------------"
-# #Check RG Status
-# $RGStatus = az group exists -n $RGName
-# # az group create -l $RGLocation -n $RGName
-
-# if ($RGStatus -eq $true)
-# {
-#     az group create -l $RGLocation -n $RGName
-# }
-# else{
-#     Write-Host resourcegroup $RGName already exists
-# }
-# #======================
-# Write-Host "End of Script ($ScriptName)"
-
 param(
-    [parameter(mandatory=$true)][string] $RgLocation,
-    [parameter(mandatory=$true)][string] $RGName
+    [Parameter(Mandatory=$true)][string] $RGLocation,
+    [Parameter(Mandatory=$true)][string] $RGName
 )
-$errorActionpreference= "stop"
-[console]::Resetcolor()
-$scriptName = $Myinvocation.Mycommand.Name
-write-host "====================================================="
-write-host resourcegrouplocation is $RgLocation
-write-host resourcegroupname is $RGName
-write-host "====================================================="
+$ErrorActionPreference  = "stop"
+[console]::ResetColor()
+$ScriptName = $MyInvocation.MyCommand.Name
 
-#checking resourcegroup status if exits or not
-$RGStatus= az group exists -n $RGName
-if($RGStatus -eq $false){
-    write-host "creating resourcegroup"
-    az group create -1 $RgLocation -n $RGName
+Write-host "----------------------------------------------------"
+Write-Host resourcegroup location is $RGLocation
+Write-Host resourcegroup name is $RGName
+Write-host "----------------------------------------------------"
+#Check RG Status
+$RGStatus = az group exists -n $RGName
+# az group create -l $RGLocation -n $RGName
 
+if ($RGStatus -eq $true)
+{
+    az group create -l $RGLocation -n $RGName
 }
 else{
-    write-host resourcegroup $RGName already exists
-    
+    Write-Host resourcegroup $RGName already exists
 }
-#====================================================
-write-host "----------------------------------------"
-write-host "-- script finished ($scriptName)"
-write-host "-----------------------------------------"
+#======================
+Write-Host "End of Script ($ScriptName)"
+
