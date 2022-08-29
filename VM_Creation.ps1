11aac1ed4cf03192bc0477b817278b7080c2ccb2
= <=>
2022-08-29 12:07:50 +0530

    Edited line - 23

--- VM_Creation.ps1
+++ VM_Creation.ps1
@@ -20,7 +20,7 @@ Write-Host "=========================================================="
 
 #Checking resourcegroup status. if exits or not
 $VMStatus = az vm list -d -o table --query "[?name=='$VmName']"
-write-Host $VMStatus
+write-Host $VmName already exists.
 # if($VMStatus -eq $true){
 #    write-Host Network $VMStatus already exists
 # }
