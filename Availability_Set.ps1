9b00bb15686c9e22af68195e5b59f40046b6bce1
= <=>
2022-09-02 12:43:21 +0530

    statement changed (IF)

--- Availability_Set.ps1
+++ Availability_Set.ps1
@@ -14,7 +14,7 @@ Write-Host "=========================================================="
 # $VMStatus = az vm list -d -o table --query "[?name=='$VmName']"
 $AvailabilityStatus = az vm availability-set list -g $RGName
 #write-Host $VmName already exists.
-if($AvailabilityStatus -eq $false){
+if($AvailabilityStatus -eq $true){
 
    Write-Host No Availability Sets found.
   
@@ -22,3 +22,5 @@ if($AvailabilityStatus -eq $false){
 else{  
         Write-host Missing Logic..
  }
+ #======================
+Write-Host "End of Script ($ScriptName)"
