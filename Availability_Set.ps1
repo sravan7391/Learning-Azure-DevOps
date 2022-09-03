02805943b8b0681813b0715e6966290ce05143c2
= <=>
2022-09-03 12:17:54 +0530

    2

--- Availability_Set.ps1
+++ Availability_Set.ps1
@@ -14,7 +14,7 @@ Write-Host "=========================================================="
 # $VMStatus = az vm list -d -o table --query "[?name=='$VmName']"
 $AvailabilityStatus = az vm availability-set list -g $RGName
 #write-Host $VmName already exists.
-if( $AvailabilityStatus -eq $true){
+if( $AvailabilityStatus -eq $false){
 
    Write-Host No Availability Sets found.
   
