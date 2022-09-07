382120231a9cb0d5c938eda8d6fcc7400c78fe57
= <=>
2022-09-07 18:33:45 +0530

    commecnted uplo line 5

--- PLB.ps1
+++ PLB.ps1
@@ -1,8 +1,8 @@
-az group create -g PLB -l centralindia
+# az group create -g PLB -l centralindia
 
- az vm create -g PLB -n VM1 --image UbuntuLTS --vnet-name PLB_Vnet --subnet PLB_Subnet --admin-username azureadmin --admin-password “testuser@123456" --size Standard_B1s --nsg PLB_NSG1 --zone 1 --public-ip-address '""'
+#  az vm create -g PLB -n VM1 --image UbuntuLTS --vnet-name PLB_Vnet --subnet PLB_Subnet --admin-username azureadmin --admin-password “testuser@123456" --size Standard_B1s --nsg PLB_NSG1 --zone 1 --public-ip-address '""'
 
- az vm create -g PLB -n VM2 --image UbuntuLTS --vnet-name PLB_Vnet --subnet PLB_Subnet --admin-username azureadmin --admin-password “testuser@123456" --size Standard_B1s --nsg PLB_NSG1 --zone 2 --public-ip-address '""'
+#  az vm create -g PLB -n VM2 --image UbuntuLTS --vnet-name PLB_Vnet --subnet PLB_Subnet --admin-username azureadmin --admin-password “testuser@123456" --size Standard_B1s --nsg PLB_NSG1 --zone 2 --public-ip-address '""'
 
 az vm availability-set create -n AVSET -g PLB --platform-fault-domain-count 3 --platform-update-domain-count 20 -l centralindia 
 
