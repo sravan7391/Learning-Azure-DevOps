1c6c691938f238c7094f9944410926667ea18e7b
= <=>
2022-09-07 18:19:15 +0530

    vm list added

--- PLB.ps1
+++ PLB.ps1
@@ -1,8 +1,8 @@
 # az group create -g PLB -l centralindia
 
-#  az vm create -g PLB -n VM1 --image UbuntuLTS --vnet-name PLB_Vnet --subnet PLB_Subnet --admin-username azureadmin --admin-password “testuser@123456" --size Standard_B1s --nsg PLB_NSG1 --zone 1 
+#  az vm create -g PLB -n VM1 --image UbuntuLTS --vnet-name PLB_Vnet --subnet PLB_Subnet --admin-username azureadmin --admin-password “testuser@123456" --size Standard_B1s --nsg PLB_NSG1 --zone 1 --public-ip-address '""'
 
-#  az vm create -g PLB -n VM2 --image UbuntuLTS --vnet-name PLB_Vnet --subnet PLB_Subnet --admin-username azureadmin --admin-password “testuser@123456" --size Standard_B1s --nsg PLB_NSG1 --zone 2
+#  az vm create -g PLB -n VM2 --image UbuntuLTS --vnet-name PLB_Vnet --subnet PLB_Subnet --admin-username azureadmin --admin-password “testuser@123456" --size Standard_B1s --nsg PLB_NSG1 --zone 2 --public-ip-address '""'
 
 # az vm availability-set create -n AVSET -g PLB --platform-fault-domain-count 2 --platform-update-domain-count 2 -l centralindia 
 
@@ -13,3 +13,5 @@
 # az vm availability-set create  --resource-group PLB  --name AVSET  --platform-fault-domain-count 2 --platform-update-domain-count 2
 
 az resource list -g PLB -o table
+
+az vm list -o table
