f4798145185d28a38b15b1d497e6dc1ecd1cb295
= <=>
2022-09-07 18:37:11 +0530

    AVSET updated

--- PLB.ps1
+++ PLB.ps1
@@ -4,7 +4,7 @@
 
 #  az vm create -g PLB -n VM2 --image UbuntuLTS --vnet-name PLB_Vnet --subnet PLB_Subnet --admin-username azureadmin --admin-password “testuser@123456" --size Standard_B1s --nsg PLB_NSG1 --zone 2 --public-ip-address '""'
 
-az vm availability-set create -n AVSET -g PLB --platform-fault-domain-count 3 --platform-update-domain-count 20 -l centralindia 
+az vm availability-set create -n AVSET -g PLB --platform-fault-domain-count 2 --platform-update-domain-count 2 -l centralindia
 
 az vm create  --resource-group PLB  --name VM3 availability-set AVSET —size Standard_B1s --vnet-name PLB_Vnet --subnet PLB_Subnet --image UbuntuLTS  --admin-username azureadmin --admin-password “testuser@123456" --nsg PLB_NSG1
 
