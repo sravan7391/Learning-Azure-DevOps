17e6dd8299b14a4e3121feab9eecf525cbedd532
= <=>
2022-09-13 12:12:56 +0530

    changes reverted

--- Vnet_Subnet_Creation.ps1
+++ Vnet_Subnet_Creation.ps1
@@ -2,11 +2,9 @@ param(
     [Parameter(Mandatory=$true)][string] $RGLocation,
     [Parameter(Mandatory=$true)][string] $RGName,
     [Parameter(Mandatory=$true)][string] $RVnetName,
-     [Parameter(Mandatory=$true)][string] $RVnetRange,
+    [Parameter(Mandatory=$true)][string] $VnetRange,
     [Parameter(Mandatory=$true)][string] $SubnetName,
-    # [Parameter(Mandatory=$true)][string] $SubnetName2,
     [Parameter(Mandatory=$true)][string] $SubnetRange
-    # [Parameter(Mandatory=$true)][string] $SubnetRange2
 )
 $ErrorActionPreference  = "stop"
 [console]::ResetColor()
@@ -22,12 +20,11 @@ $RGStatus1 = az network vnet list -o table --query "[?name=='$RVnetName']"
 
 if ($RGStatus1 -eq "")
 {
-    az network vnet create -g $RGName -n $RVnetName --address-prefix $VnetRange --subnet-name $SubnetName --subnet-prefix $SubnetRange -l $RGLocation   
+    az network vnet create -g $RGName --vnet-name $RVnetName --address-prefix $VnetRange --subnet-name $SubnetName --subnet-prefix $SubnetRange -l $RGLocation   
 }
 else {
     write-Host Network $RGName, $RVnetName already exists   
-    az network vnet subnet list -g $RGName --vnet-name $RVnetName -o table
+    az network vnet subnet list -g $RGName --vnet-name $RVnetName
     # az network vnet list --resource-group $RGName -o table
 }
-    # az network vnet list --resource-group $RGName -o table
 write-Host "----------------- End of Script ($ScriptName) -----------------"
