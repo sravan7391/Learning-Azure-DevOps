750757c9a3d7e78f4049507f3faff125ceaeca9e
= <=>
2022-09-16 19:46:00 +0530

    vnet, subnet changed in parameters

--- VM_Creation.ps1
+++ VM_Creation.ps1
@@ -1,7 +1,7 @@
 param(
    [Parameter(Mandatory=$true)][string] $RGName,
-   [Parameter(Mandatory=$true)][string] $VnetName2,
-   [Parameter(Mandatory=$true)][string] $SubnetName2,
+   [Parameter(Mandatory=$true)][string] $VnetName,
+   [Parameter(Mandatory=$true)][string] $SubnetName,
    [Parameter(Mandatory=$true)][string] $Urn,
    [Parameter(Mandatory=$true)][string] $VmName,
    [Parameter(Mandatory=$true)][string] $Username,
@@ -14,8 +14,8 @@ $ErrorActionPreference = "Stop"
 $scriptName = $MyInvocation.MyCommand.Name
 write-host "========================================================="
 write-host resourcegroupname is $RGName
-write-host vnetname is $VnetName2
-write-host SubnetName is $SubnetName2
+write-host vnetname is $VnetName
+write-host SubnetName is $SubnetName
 Write-Host "=========================================================="
 
 #Checking resourcegroup status. if exits or not
@@ -25,7 +25,7 @@ $VMStatus =  az vm list -d -o table --query "[?name=='$VmName']"
 if($VMStatus -eq ""){
 
    Write-Host creating VM
-   az vm create --resource-group $RGName --name $VmName --image $Urn --vnet-name $VnetName2 --subnet $SubnetName2 --admin-username $Username --admin-password $Password --size $VmSize --public-ip-sku Standard
+   az vm create --resource-group $RGName --name $VmName --image $Urn --vnet-name $VnetName --subnet $SubnetName --admin-username $Username --admin-password $Password --size $VmSize --public-ip-sku Standard
    az vm list -g $RGName -o table
   
 }
