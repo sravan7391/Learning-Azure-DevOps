aa727ea7b566bce2ea1aac00d0f01f2ac9a2a4f1
= <=>
2022-09-16 20:06:27 +0530

    Vnet peering code added

--- Vnet_Peering.ps1
+++ Vnet_Peering.ps1
@@ -1,7 +1,10 @@
 param(
    [Parameter(Mandatory=$true)][string] $RGName,
    [Parameter(Mandatory=$true)][string] $VnetName,
-   [Parameter(Mandatory=$true)][string] $BastionVnet
+   [Parameter(Mandatory=$true)][string] $BastionVnet,
+   [Parameter(Mandatory=$true)][string] $LinkName1,
+   [Parameter(Mandatory=$true)][string] $LinkName2
+
   
 
 )
@@ -11,21 +14,29 @@ $scriptName = $MyInvocation.MyCommand.Name
 write-host "========================================================="
 write-host resourcegroupname is $RGName
 write-host vnetname is $VnetName
-write-host Vnet is $SBastionVnet
+write-host Vnet is $BastionVnet
 Write-Host "=========================================================="
 
 #Checking vnet status. if exits or not
 
-$VnetPeeringStatus = az network vnet list
+$VnetPeeringStatus = az network vnet list --query "[?name=='$RGName']"
 
 if($VnetPeeringStatus -contains $VnetName){
    az network vnet list -o table
-   az network vnet peering create -g MyResourceGroup -n MyVnet1ToMyVnet2 --vnet-name $VnetName --remote-vnet $BastionVnet --allow-vnet-access
-   az network vnet peering show -g $RGName -n MyVnet1ToMyVnet2 --vnet-name $VnetName
+   az network vnet peering create -g $RGName -n $LinkName --vnet-name $VnetName --remote-vnet $BastionVnet --allow-vnet-access
+}
+else {
+   Write-Error Failed to Create Network Peering
+}
+
+if($VnetPeeringStatus -contains $VnetName){
+   az network vnet list -o table
+   az network vnet peering create -g $RGName -n $LinkName2 --vnet-name $BastionVnet --remote-vnet $VnetName --allow-vnet-access
 }
 else {
    az network vnet peering list -g $RGName --vnet-name $VnetName
 }
+
 #==================================================================
 
 Write-Host "----------------------------------------------------"
