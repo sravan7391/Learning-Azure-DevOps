24f8002825e1aa47bbb21c66b3b8a932be3c00b0
= <=>
2022-09-16 10:19:14 +0530

    vnet peering IaC

--- Vnet_Peering.ps1
+++ Vnet_Peering.ps1
@@ -1,13 +1,8 @@
 param(
    [Parameter(Mandatory=$true)][string] $RGName,
    [Parameter(Mandatory=$true)][string] $VnetName,
-   [Parameter(Mandatory=$true)][string] $SubnetName,
-   [Parameter(Mandatory=$true)][string] $SubnetName2,
-   [Parameter(Mandatory=$true)][string] $Urn,
-   [Parameter(Mandatory=$true)][string] $VmName,
-   [Parameter(Mandatory=$true)][string] $Username,
-   [Parameter(Mandatory=$true)][string] $Password,
-   [Parameter(Mandatory=$true)][string] $VmSize
+   [Parameter(Mandatory=$true)][string] $BastionVnet
+  
 
 )
 $ErrorActionPreference = "Stop"
@@ -16,33 +11,21 @@ $scriptName = $MyInvocation.MyCommand.Name
 write-host "========================================================="
 write-host resourcegroupname is $RGName
 write-host vnetname is $VnetName
-write-host SubnetName is $SubnetName2
+write-host Vnet is $SBastionVnet
 Write-Host "=========================================================="
 
-#Checking resourcegroup status. if exits or not
+#Checking vnet status. if exits or not
 
-$VMStatus =  az vm list -d -o table --query "[?name=='$VmName']" 
-#write-Host $VmName already exists.
-if($VMStatus -eq "")
-{
-   Write-Host creating VM
-   az vm create --resource-group $RGName --name $VmName --image $Urn --vnet-name $VnetName --subnet $SubnetName2 --admin-username $Username --admin-password $Password --size $VmSize --public-ip-sku Standard
-   az vm list -g $RGName -o table
-  
+$VnetPeeringStatus = az network vnet list --query "[?name=='$RVnetName']"
+
+if($VnetPeeringStatus -ne ""){
+   az network vnet list -o table
+   az network vnet peering create -g MyResourceGroup -n MyVnet1ToMyVnet2 --vnet-name $VnetName --remote-vnet $BastionVnet --allow-vnet-access
+   az network vnet peering show -g $RGName -n MyVnet1ToMyVnet2 --vnet-name $VnetName
 }
-else{  
-
-    write-Host $VmName already exists
-    az vm list -g $RGName -o table
- }
-
-# if($VMStatus -eq $false){
-#    Write-Host creating VM
-#    az vm create --resource-group $RGName --name $VmName --image $Urn --vnet-name $VnetName --subnet $SubnetName --admin-username $Username --admin-password $Password --size $VmSize
-# }
-# else{
-#    az vm list -d -o table --query "[?name=='$VmName']"
-# }
+else {
+   az network vnet peering list -g $RGName --vnet-name $VnetName
+}
 #==================================================================
 
 Write-Host "----------------------------------------------------"
